48f4c1621b86605798fbcb10f094e925a3e5cc7c
Rahsan Lewis <rahsanlewis@gmail.com>
2022-10-19 21:59:52 -0400

    modified license and javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,32 +10,26 @@ const apiKey1 = '3aOkUhqhHeSCKZu7WjMvBl1hPZu2xPSH'
 //wallhaven apiKey
 const apiKey2 = 'kDzUKzeCUb16O7WleQ9GG7GUMXkVOii0'
 
-searchBtn.addEventListener('click', search);
-
-function search() {
-	fetch("http://api.giphy.com/v1/gifs/search?q=" + searchInput + "&api_key=3aOkUhqhHeSCKZu7WjMvBl1hPZu2xPSH&limit=3")
-		.then(response => response.JSON())
-		.then(data => {
-			search_result.innerHTML = `<img src=${data.file} alt="wallpaper 1" />`
-		}
-    
-    );
-}
-
-
 
 //search button functionality (API calls within eventListener to prevent calls from happening on search page)
 searchBtn.addEventListener("click", function () {
-  //brings user to results HTML page from index
-  window.location.replace("results.html")
 
-
-  // for loop setup for API call 1 
-  for (i = 0; i < 3; i++) {
-    //creates containers for gif outputs
-    const gifContainer = document.createElement("")
-  }
-
+  fetch(`http://api.giphy.com/v1/gifs/search?q=${searchInput.value}&api_key=${apiKey1}&limit=3`)
+    .then(response => response.JSON())
+    .then(gifData => {
+      fetch(`wallpaperapi`)
+        .then(response => response.JSON())
+        .then(wallpaperData => {
+          // manipulate DOM here
+        })
+      // for loop setup for API call 1 
+      for (i = 0; i < 3; i++) {
+        //creates containers for gif outputs
+        const gifContainer = document.createElement("")
+      }
+      search_result.innerHTML = `<img src=${data.file} alt="wallpaper 1" />`
+    }
+    );
 
   //for loop setup for API call 2
   for (i = 0; i < 3; i++) {
